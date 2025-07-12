0c501c91e0a0f74f797954876b05c8f40b390a54
vibercoderting <dexterjedidi@gmail.com>
2025-07-12 11:07:46 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+function increment() {
+  count++;
+  document.getElementById("count").innerText = count;
+}
+
+function reset() {
+  count = 0;
+  document.getElementById("count").innerText = count;
+}
